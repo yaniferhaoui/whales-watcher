cf98abdea71517115f4d5fc5d83b32107a684b4d
Yani Ferhaoui <yani.ferhaoui@hotmail.fr>
2022-01-06 17:28:49 +0100

    feat: filter tx through input data

---
+++ colletor/contract/Swap.py
@@ -0,0 +1,6 @@
+class Swap:
+
+    def __init__(self, token, is_buy, amount):
+        self.token = token
+        self.is_buy = is_buy
+        self.amount = amount
--- colletor/contract/SwapContract.py
+++ colletor/contract/SwapContract.py
@@ -1,20 +1,14 @@
 class SwapContract:
 
-    def __init__(self, label, contract_hash, transfer_topic_hash):
+    def __init__(self, label, contract_hash, transfer_topic_hash, abi):
         self.label = label
         self.contract_hash = contract_hash
         self.transfer_topic_hash = transfer_topic_hash
+        self.abi = abi
 
     def contract_match(self, transaction):
         return transaction.to == self.contract_hash
 
-    # TODO : Is method topic always at the index 0 ?
     def is_transfer(self, transaction_receipt_log):
         topics = transaction_receipt_log['topics']
         return len(topics) != 0 and self.transfer_topic_hash == topics[0].hex()
-
-    # def is_transfer(self, transaction_receipt_log):
-    #     for topic in transaction_receipt_log['topics']:
-    #         if topic.hex() == self.transfer_topic_hash:
-    #             return True
-    #     return False
